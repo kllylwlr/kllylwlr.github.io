6b2edd138f389c464a2cb8458f6f2f934167f4ca
kllylwlr <kllylwlr@gmail.com>
2024-04-03 23:10:08 -0400

    Updated functionality of linefinder case study button

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ linefinderPrototypeBtn.addEventListener("click", function() {
 let bakeXpressPrototypeBtn = document.getElementById("bakeXpressPrototypeBtn");
 bakeXpressPrototypeBtn.addEventListener("click", function() {
   //location.href = "https://www.figma.com/proto/zgz5G84ejt4veLf3XidNzr/Line-Finder---Interactable-Prototype?node-id=1790-84430&starting-point-node-id=1790%3A84430&mode=design&t=TxzzYShbwsH9WoQs-1"
-	window.open("https://www.figma.com/proto/yRxswLAyuiREdROgyvQUV1/Bake-XPress-Interactable-Prototype?type=design&node-id=1-4&t=4cA4Q0ztu655aVgK-1&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A4&show-proto-sidebar=1&mode=design", "_blank");
+	window.open("https://www.figma.com/proto/yRxswLAyuiREdROgyvQUV1/Bake-XPress-Interactable-Prototype?type=design&node-id=1-4&t=h094TKLxt3xEGnCk-1&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A4&show-proto-sidebar=1&mode=design", "_blank");
 });
