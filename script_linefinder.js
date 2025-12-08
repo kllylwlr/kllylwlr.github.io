d5e771d71fd73205c0f6e0297331c88652356edd
Kelly Lawlor <97195501+kllylwlr@users.noreply.github.com>
2025-12-07 21:10:28 -0500

    Add files via upload
    Edited code to improve website accessibility

---
+++ script_linefinder.js
@@ -0,0 +1,6 @@
+//Linefinder Prototype Button
+let linefinderPrototypeBtn = document.getElementById("linefinderPrototypeBtn");
+linefinderPrototypeBtn.addEventListener("click", function() {
+  //location.href = "https://www.figma.com/proto/zgz5G84ejt4veLf3XidNzr/Line-Finder---Interactable-Prototype?node-id=1790-84430&starting-point-node-id=1790%3A84430&mode=design&t=TxzzYShbwsH9WoQs-1"
+	window.open("https://www.figma.com/proto/zgz5G84ejt4veLf3XidNzr/Line-Finder---Interactable-Prototype?node-id=1790-84430&starting-point-node-id=1790%3A84430&mode=design&t=TxzzYShbwsH9WoQs-1", "_blank");
+});
