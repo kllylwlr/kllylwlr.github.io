f0e4aa2bcb4a431884b57c2f95867f2d0a67e82c
Kelly Lawlor <97195501+kllylwlr@users.noreply.github.com>
2026-08-23 18:50:52 -0400

    Add files via upload
    Added internship work for Arizona Beverages and updated interactive prototype buttons

---
+++ script_arizona.js
@@ -0,0 +1,6 @@
+//Linefinder Prototype Button
+let linefinderPrototypeBtn = document.getElementById("arizonaPrototypeBtn");
+linefinderPrototypeBtn.addEventListener("click", function() {
+  //location.href = "https://www.figma.com/proto/16QzB1gj2Y7YaYejLNinJ4/KL-Portfolio---Arizona-Iced-Tea?node-id=1-657&t=FlzaPmWWSWg2PGyD-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
+	window.open("https://www.figma.com/proto/16QzB1gj2Y7YaYejLNinJ4/KL-Portfolio---Arizona-Iced-Tea?node-id=1-657&t=FlzaPmWWSWg2PGyD-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1", "_blank");
+});
